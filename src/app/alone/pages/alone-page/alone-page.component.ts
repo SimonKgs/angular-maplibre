73fce7c6bd683188437a33554c0fd37b1f2356a0
kgs <simonudemy09@gmail.com>
2024-10-04 12:31:51 +0200

    tests with standalone components

---
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { CounterAloneComponent } from "../../components/counter-alone/counter-alone.component";
+
+
+// this component can works by itself, it doent'n need to be imported in a module
+// it is like its own module
+// the only thing it needs to be an standalone component is to have the property on true
+// It let the component import its dependencies as a module to use it
+// eg: to use angular directives I am importing CommonModule
+// to use a use a standAlone component inside another standalone like this one
+// I need to import each one of the in the imports of this module, 
+// otherwise they will be unknow components
+@Component({
+  selector: 'app-alone-page',
+  standalone: true,
+  imports: [CommonModule, CounterAloneComponent],
+  templateUrl: './alone-page.component.html',
+  styleUrls: ['./alone-page.component.css']
+})
+export class AlonePageComponent {
+
+  // I can inject dependencies but it must be provideInRoot or imported here from a module
+  constructor(){}
+}
